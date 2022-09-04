7a93bc0503dcdd6bd8578bcf3f31a1194a9b6d74
Saurya mishra <60350420+sauryamishra@users.noreply.github.com>
2022-09-04 13:50:30 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,25 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		int age = 23;
+		
+		char myName = 's';
+		
+		float decimal = 13.14F;
+		
+		long anyNumber = 12345678912L;
+		
+		System.out.println(age);
+		System.out.println(myName);
+		System.out.println(decimal);
+		System.out.println(anyNumber);
+		
+		
+		
+		
+
+	}
+
+}
